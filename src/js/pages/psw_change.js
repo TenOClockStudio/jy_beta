56c418a194141a10e02cc494c2b9a03e913f2259
chaosfox2013 <461813957@qq.com>
2016-02-07 16:49:50 +0800

    密码

--- src/js/pages/psw_change.js
+++ src/js/pages/psw_change.js
@@ -1,7 +1,22 @@
 define(function (require) {
     'use strict';
     require('jquery');
+    require('weui_js');
     var global = require('common/global'),
         http = require('common/http');
-	console.log("psw");
+    $('#submit').click(function(){
+    	var a = $('.psw').val();
+    	var b = $('.psw1').val();
+    	var c = $('.psw2').val();
+    	if (a=="") {
+    		$.alert("原密码不能为空");
+    	}else if (b=="") {
+    		$.alert("新密码不能为空");
+    	}else if (c=="") {
+    		$.alert("确认密码不能为空");
+    	}else{
+    		$.alert("修改密码成功");
+    	}
+		
+    });
 });
