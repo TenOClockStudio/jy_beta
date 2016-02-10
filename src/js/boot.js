91c58ea4aaa414a9da2285bc45c5d2700ee8e953
libbyyounh <libbyyounh@gmail.com>
2016-02-10 16:23:35 +0800

    addtendance

--- src/js/boot.js
+++ src/js/boot.js
@@ -6,7 +6,8 @@ requirejs.config({
         "jquery": "js/lib/jquery-2.1.4",
         "weui_js":"js/lib/jquery-weui",
         "common": "js/common",
-        "circliful": "js/lib/jquery.circliful.min"
+        "circliful": "js/lib/jquery.circliful.min",
+        "mobiscroll_datatime":"js/lib/mobiscroll_datetime/mobiscroll.custom-2.5.2.min"
         //"plugins": "common/plugins",
         //"templates": "templates",
         //"page" : "page"
@@ -14,6 +15,7 @@ requirejs.config({
     shim: {
         'jquery':{'exports':'$'},
         "weui_js":{deps: ['jquery']},
-        "circliful":{deps: ['jquery']}
+        "circliful":{deps: ['jquery']},
+        "mobiscroll_datatime":{deps: ['jquery']}
     }
 });
