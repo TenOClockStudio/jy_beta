d1c731954714dc1c8fff15843c8d42d6713f8ba8
libbyyounh <libbyyounh@gmail.com>
2016-02-12 17:26:55 +0800

    video

--- src/js/pages/video.js
+++ src/js/pages/video.js
@@ -4,4 +4,15 @@ define(function (require) {
     var global = require('common/global'),
         http = require('common/http');
 	console.log("vedio");
+
+    $(".video-nav .nav-ct").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+        var target = $(this).attr("targetid");
+        $(".video-tab-wrap .video-ct").each(function(){
+            $(this).addClass("hide");
+            if(this.id===target){
+                $(this).removeClass("hide");
+            }
+        });
+    });
 });
