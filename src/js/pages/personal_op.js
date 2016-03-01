857a716adb936b2b1fc897dade140efb91cd02b0
libbyyounh <libbyyounh@gmail.com>
2016-03-01 11:23:18 +0800

    add op

--- src/js/pages/personal_op.js
+++ src/js/pages/personal_op.js
@@ -4,4 +4,8 @@ define(function (require) {
     var global = require('common/global'),
         http = require('common/http');
 	console.log("my_n");
+    $("#top-icon-menu").click(function(){
+        $('.top-trig').toggleClass("hide");
+        $('.top-menu-list').toggleClass("hide");
+    });
 });
